9d40b2b8ac44184101fab2c67f8310d1bb9ddb58
Egidijus Bockus <egidijusbockus@gmail.com>
2021-09-23 08:44:49 +0300

    Server

---
+++ README.md
@@ -0,0 +1,13 @@
+![LICENSE](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
+
+# Yelp Application
+
+This project is for educational porpuses only.
+
+Site published at
+
+## Project features
+
+## Authors
+
+[Egidijus](https://github.com/full-stack-nodejs)
---
+++ server/server.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// Get all Restourants
+app.get("/api/v1/restourants", (req, res) => {
+  console.log("router run");
+  res.status(200).json({
+    status: "success",
+    data: {
+      restourant: ["mcdonalds", "wendys"],
+    },
+  });
+});
+
+// Get a Restourant
+app.get("/api/v1/restourants/:id", (req, res) => {
+  console.log(req.params);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      restourant: ["mcdonalds", "wendys"],
+    },
+  });
+});
+
+// Create a Restourant
+app.post("/api/v1/restourants", (req, res) => {
+  console.log(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      restourant: ["mcdonalds", "wendys"],
+    },
+  });
+});
+
+// Update a Restourant
+app.put("/api/v1/restourants/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      restourant: ["mcdonalds", "wendys"],
+    },
+  });
+});
+
+//Delete Restourant
+app.delete("/api/v1/restourants/:id", (req, res) => {
+  console.log("Delete");
+
+  res.status(204).json({
+    status: "success",
+  });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server run and listening on port ${port}`);
+});
