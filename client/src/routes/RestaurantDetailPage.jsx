83180572176410def0cce00f2ba0b880f83d7904
Egidijus Bockus <egidijusbockus@gmail.com>
2021-09-26 19:38:23 +0300

    Reviews

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router";
 import RestaurantFinder from "../apis/RestaurantFinder";
+import Reviews from "../components/Reviews";
 import StarRating from "../components/StarRating";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 
@@ -20,7 +21,13 @@ const RestaurantDetailPage = () => {
     };
     fetchData();
   }, []);
-  return <div>{selectedRestaurant && <StarRating rating={1.1} /> }</div>;
+  return <div>{selectedRestaurant && (
+    <>
+    <div className="mt-3">
+      <Reviews />
+    </div>
+    </>
+  )}</div>;
 };
 
 export default RestaurantDetailPage;
